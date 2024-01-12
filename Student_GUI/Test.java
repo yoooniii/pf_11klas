a20f494a6d9b3ade1eedfad7d77f35697834895a
meow <116832176+yoooniii@users.noreply.github.com>
2024-01-12 17:35:58 +0200

    Create Test.java

---
+++ Student_GUI/Test.java
@@ -0,0 +1,14 @@
+package studentProject;
+
+public class test {
+
+ public static void main(String[] args) {
+ /*egn e=new egn(645027720L);
+ System.out.println(e.isCorrect());
+ System.out.println(e.sex());
+ System.out.println(e.birthDate());*/
+ student s=new student("Koko","Pilev","Kokoshkov","0608057724");
+ System.out.println(s);
+ }
+
+}
